13fe9b82064337ee1c9bd7a5f69a6f0a65c238a8
Personala <y_ishiwatari@hotmail.co.jp>
2023-11-25 23:55:56 +0900

    add set param

--- inc/test.h
+++ inc/test.h
@@ -1,2 +1,6 @@
+#include <stdbool.h>
+
 // sub function
 void print_sub(void);
+void set_int_param(int prm);
+void set_bool_param(bool prm);
--- src/sub.c
+++ src/sub.c
@@ -7,3 +7,13 @@ void print_sub(void)
     // test print
     printf("test print ...\n");
 }
+
+void set_int_param(int prm)
+{
+    int_param = prm;
+}
+
+void set_bool_param(bool prm)
+{
+    bool_param = prm;
+}
