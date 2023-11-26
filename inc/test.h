6c5febe0bce3d1e0330ab858cfc1f2fc967dff8c
Personala <y_ishiwatari@hotmail.co.jp>
2023-11-26 20:07:11 +0900

    add main loop

--- inc/test.h
+++ inc/test.h
@@ -1,7 +1,16 @@
+#ifndef __TEST__
+#define __TEST__
+
 #include <stdbool.h>
+
+#define LOOP_CONTINUE 0
+#define LOOP_END -1
 
 // sub function
 void print_sub(void);
 void print_param(void);
 void set_int_param(int prm);
 void set_bool_param(bool prm);
+int main_loop(void);
+
+#endif  // __TEST__
--- inc/test_param.h
+++ inc/test_param.h
@@ -1,6 +1,11 @@
+#ifndef __TEST_PARAM__
+#define __TEST_PARAM__
+
 #include <stdbool.h>
 
 // test param
 
 int int_param = 0;
 bool bool_param = false;
+
+#endif  // __TEST_PARAM
